5dbd90da788ecb8083a7566af08bf197272ea261
kenzz-sz <r4mseiandsei@gmail.com>
2026-01-13 13:34:01 +0700

    Refactor data fetching to use Promise.all

--- xgz-config-vip-emuconsole.js
+++ xgz-config-vip-emuconsole.js
@@ -1,23 +1,27 @@
 (function() {
+  function initGUI() {
   window.startup = function(){
     bot(`
     ${dataxgzconfig.namexgzconfig}
     ${dataxgzconfig.lastupdatedxgzconfig}
     `)
   };
-  
+  system("clear")
+  d(1000, "startup()")
+}
   const dataxgzconfig = {
     "namexgzconfig": "",
     "lastupdatedxgzconfig": ""
   }
-  fetch("https://raw.githubusercontent.com/kenzz-sz/XGZ/refs/heads/main/lastupdated.txt")
-  .then(res => res.text())
-  .then(text => (dataxgzconfig.lastupdatedxgzconfig) = (text));
-  fetch("https://raw.githubusercontent.com/kenzz-sz/XGZ/refs/heads/main/namexgz.txt")
-  .then(res => res.text())
-  .then(text => (dataxgzconfig.namexgzconfig) = (text));
   
-  system("clear")
-  d(1000, "startup()")
+Promise.all([
+  fetch("https://raw.githubusercontent.com/kenzz-sz/XGZ/refs/heads/main/lastupdated.txt").then(r => r.text()),
+  fetch("https://raw.githubusercontent.com/kenzz-sz/XGZ/refs/heads/main/namexgz.txt").then(r => r.text())
+]).then(([last, name]) => {
+  dataxgzconfig.lastupdatedxgzconfig = last.trim();
+  dataxgzconfig.namexgzconfig = name.trim();
+
+  initGUI(); // ← BARU JALANKAN GUI
+});
 })();
 
