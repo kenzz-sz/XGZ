7c74b25400be2f8b1c3149cc44131edcd5c85a3d
kenzz-sz <r4mseiandsei@gmail.com>
2026-01-13 13:19:44 +0700

    Fetch additional configuration data from remote source

--- xgz-config-vip-emuconsole.js
+++ xgz-config-vip-emuconsole.js
@@ -13,6 +13,10 @@
   fetch("https://raw.githubusercontent.com/kenzz-sz/XGZ/refs/heads/main/lastupdated.txt")
   .then(res => res.text())
   .then(text => (dataxgzconfig.lastupdatedxgzconfig) = (text));
+  fetch("https://raw.githubusercontent.com/kenzz-sz/XGZ/refs/heads/main/namexgz.txt")
+  .then(res => res.text())
+  .then(text => (dataxgzconfig.namexgzconfig) = (text));
+  
   system("clear")
   d(1000, "startup()")
 })();
