c9351a178db9379e2805e0ffd69b251f59cc550d
kenzz-sz <r4mseiandsei@gmail.com>
2026-01-13 13:04:24 +0700

    Fix syntax errors in xgz-config-vip-emuconsole.js

--- xgz-config-vip-emuconsole.js
+++ xgz-config-vip-emuconsole.js
@@ -3,6 +3,9 @@
     "namexgzconfig": "",
     "lastupdatedxgzconfig": ""
   }
+  fetch("https://raw.githubusercontent.com/kenzz-sz/XGZ/refs/heads/main/lastupdated.txt")
+  .then(res => res.text())
+  .then(text => (dataxgzconfig.lastupdatedxgzconfig) = (text));
   system("clear")
   d(1000, "startup()")
   
